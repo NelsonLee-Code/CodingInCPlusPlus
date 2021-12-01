72a005dcbeeb56ea73c6a226827b71ae1fa6d36e
NelsonLee-Code <85120900+NelsonLee-Code@users.noreply.github.com>
2021-12-01 15:49:35 -0500

    Create globals.h

---
+++ ShapesLinkedList/globals.h
@@ -0,0 +1,18 @@
+//
+//  globals.h
+//  
+
+
+#ifndef globals_h
+#define globals_h
+
+#include <iostream>
+using namespace std;
+#include<string>
+
+#define NUM_KEYWORDS 6
+string keyWordsList[NUM_KEYWORDS]={"shape", "group", "move", "delete", "draw", "pool"};
+
+#define NUM_TYPES 3
+string shapeTypesList[NUM_TYPES]={"ellipse", "rectangle","triangle"};
+#endif 
