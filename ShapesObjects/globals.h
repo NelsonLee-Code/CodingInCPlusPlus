0dedb6f889e6e6ba26be13dcc930b389c4829072
NelsonLee-Code <85120900+NelsonLee-Code@users.noreply.github.com>
2021-12-01 15:33:34 -0500

    Create globals.h

---
+++ ShapesObjects/globals.h
@@ -0,0 +1,15 @@
+//
+//  globals.h
+//  
+
+#ifndef globals_h
+#define globals_h
+#include<string>
+
+#define NUM_KEYWORDS 7
+string keyWordsList[7]={"all", "maxShapes", "create", "move", "rotate", "draw", "delete"};
+
+#define NUM_TYPES 4
+string shapeTypesList[4]={"circle", "ellipse", "rectangle","triangle"};
+
+#endif 
