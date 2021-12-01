a48d9fe173ae972df16523f0b972162b26a15297
NelsonLee-Code <85120900+NelsonLee-Code@users.noreply.github.com>
2021-12-01 15:28:16 -0500

    Create globals.h

---
+++ TicTacToe/globals.h
@@ -0,0 +1,18 @@
+//
+//  globals.h
+//
+
+#ifndef globals_h
+#define globals_h
+
+#define X     1
+#define O    -1
+#define Empty 0
+
+#define boardSize 3
+
+class GameState;
+
+void playMove(GameState&);
+
+#endif 
