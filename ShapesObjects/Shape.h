0b7d8fcea5e9d60ac3b963b3d201ff86cfa533ef
NelsonLee-Code <85120900+NelsonLee-Code@users.noreply.github.com>
2021-12-01 15:46:12 -0500

    Create Shape.h

---
+++ ShapesObjects/Shape.h
@@ -0,0 +1,46 @@
+//
+//  Shape.h
+//  
+
+#ifndef Shape_h
+#define Shape_h
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Shape 
+{
+    private:
+        string name;                  
+        string type;                  
+        int x_location;              
+        int y_location;             
+        int x_size;                    
+        int y_size;                  
+        int rotation = 0;              
+    
+    public:
+        Shape(string n, string t, int x_loc, int x_sz, int y_loc, int y_sz);
+
+        string getType();             
+        string getName();              
+        int getXlocation();      
+        int getYlocation();            
+        int getXsize();             
+        int getYsize();          
+
+        void setType(string t);        
+        void setName(string n);      
+        void setXlocation(int x_loc);  
+        void setYlocation(int y_loc);  
+        void setXsize(int x_sz);       
+        void setYsize(int y_sz);       
+
+        void setRotate(int angle);    
+
+        void draw();                   
+};
+
+#endif
+
